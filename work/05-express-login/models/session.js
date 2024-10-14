fa1c7ef427ea768a0e51c40116a83ec50be9471b
wanshima <ma.wans@northeastern.edu>
2024-10-13 18:16:02 -0700

    Adds Assignment 5

---
+++ work/05-express-login/models/session.js
@@ -0,0 +1,35 @@
+const sessions = {}; 
+const profiles = {};
+
+function getSession(sid) {
+  return sessions[sid];
+}
+
+function createSession(sid, username) {
+  sessions[sid] = { username };
+  if (!profiles[username]) {
+    profiles[username] = { storedWord: '' }; 
+  }
+}
+
+function deleteSession(sid) {
+  delete sessions[sid];
+}
+
+function getUserProfile(username) {
+  return profiles[username] || { storedWord: '' };
+}
+
+function updateStoredWord(username, newWord) {
+  if (profiles[username]) {
+    profiles[username].storedWord = newWord;
+  }
+}
+
+module.exports = {
+  getSession,
+  createSession,
+  deleteSession,
+  getUserProfile,
+  updateStoredWord
+};
